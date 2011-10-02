6e5d6683188584c915e0c03c3b52e48c67f60b76
ch33kybutt <ch33kybutt@yahoo.com>
2011-10-02 21:55:01 +0800

    Removed redundant call

--- full_passion.mk
+++ full_passion.mk
@@ -20,7 +20,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/htc/passion/passion_us.mk)
